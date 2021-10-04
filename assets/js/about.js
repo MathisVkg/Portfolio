baeb54d8521d6f88db01aab381ab07e9a9b91d0a
MathisVkg <mvk181@gmail.com>
2021-10-04 14:09:32 +0200

    edit

--- assets/js/about.js
+++ assets/js/about.js
@@ -4,21 +4,59 @@ let navTag = document.getElementById('navTarget');
 let navTag2 = document.getElementById('navTarget2');
 let navButtonOpen = document.getElementById('navButtonOpen');
 let navButtonClose = document.getElementById('navButtonClose');
+let header = document.getElementById('headerTagert');
+let arrow = document.getElementById('arrow1');
+let arrow2 = document.getElementById('arrow2');
+let arrow3 = document.getElementById('arrow3');
+let body = document.body;
 
+arrow.addEventListener('click', () => {
+    body.classList.remove('bodyClose');
+})
+arrow2.addEventListener('click', () => {
+    body.classList.remove('bodyClose');
+})
+arrow3.addEventListener('click', () => {
+    body.classList.remove('bodyClose');
+})
 
 
 navButtonOpen.addEventListener('click', () => {
-    navTag.classList.remove('navOff');
-    navTag.classList.add('navOn');
-    navTag2.classList.remove('navOff2');
-    navTag2.classList.add('navOn2');
-    navButtonOpen.setAttribute('style', 'z-index: 0');
+    header.setAttribute('style', 'z-index: 1');
+    // header.setAttribute('style', 'display: flex');
+    // setTimeout(function() {
+        navTag.classList.remove('navOff');
+        navTag2.classList.remove('navOff2');
+
+        navTag.classList.add('navOn');
+        navTag2.classList.add('navOn2');
+    // }, 20);
+    navButtonOpen.style.zIndex = null;
 })
 navButtonClose.addEventListener('click', () => {
     navTag.classList.remove('navOn');
-    navTag.classList.add('navOff');
     navTag2.classList.remove('navOn2');
+
+    navTag.classList.add('navOff');
     navTag2.classList.add('navOff2');
+    // setTimeout(function() {
+    //     header.setAttribute('style', 'display: none');
+    // }, 1000);
+    header.style.zIndex = -1;
     navButtonOpen.setAttribute('style', 'z-index: 1');
 })
 
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+});
