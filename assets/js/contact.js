c1735be966ef72ece61f991d4f003c6527da8aa1
MathisVkg <mvk181@gmail.com>
2021-10-05 16:54:18 +0200

    edit

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -30,18 +30,3 @@ navButtonClose.addEventListener('click', () => {
     header.style.zIndex = -1;
     navButtonOpen.setAttribute('style', 'z-index: 1');
 })
-
-
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-          window.location.hash = hash;
-        });
-      }
-    });
-});
